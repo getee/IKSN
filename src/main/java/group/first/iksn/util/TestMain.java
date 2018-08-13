51ab8dd4e85ebe2b3f623547e6ff28da4d1c94f2
getee <634630676@qq.com>
2018-08-13 09:15:43 +0800

    上传界面以及参数的确定

--- src/main/java/group/first/iksn/util/TestMain.java
+++ src/main/java/group/first/iksn/util/TestMain.java
@@ -3,5 +3,11 @@ package group.first.iksn.util;
 public class TestMain {
     public static void main(String[] args) {
 
+        String rootPath="D:\\AppSCM\\IKSN\\out\\artifacts\\IKSN_war_exploded\\";
+        rootPath=rootPath.substring(0,rootPath.lastIndexOf('\\'));
+        String filepath=rootPath.substring(0,rootPath.lastIndexOf('\\'))+"/resource/";
+
+        System.out.println("rootPath:" + rootPath);
+        System.out.println("path:" + filepath);
     }
 }
