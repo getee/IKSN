8932e4f9720b25157368dd79e427c76079da30d5
getee <634630676@qq.com>
2018-08-14 15:56:20 +0800

    上传功能完成

--- src/main/java/group/first/iksn/service/ResourceService.java
+++ src/main/java/group/first/iksn/service/ResourceService.java
@@ -1,7 +1,9 @@
 package group.first.iksn.service;
 
 import group.first.iksn.model.bean.CollectResource;
+import group.first.iksn.model.bean.Resource;
 import group.first.iksn.model.bean.ResourceComments;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 public interface ResourceService {
     //评论评分
@@ -13,4 +15,10 @@ public interface ResourceService {
 
 
     //查询下载次数
+
+    //上传文件，在service检测重复
+    boolean checkResource(CommonsMultipartFile f,String filePath);
+
+    //资源表和资源标签表操作，resource.path
+    boolean upLoadResourc(Resource resource,String[] rTag);
 }
