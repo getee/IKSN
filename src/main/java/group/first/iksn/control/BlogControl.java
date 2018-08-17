5295e89669110238f7552e96e37560b776e8f841
dj7813 <1028221450@qq.com>
2018-08-17 09:34:44 +0800

    session导包

--- src/main/java/group/first/iksn/control/BlogControl.java
+++ src/main/java/group/first/iksn/control/BlogControl.java
@@ -21,6 +21,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
